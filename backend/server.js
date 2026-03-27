b9deb1d920d193404c74ac82380972b43a63231b
angelkibui <a.kibui@alustudent.com>
2026-03-27 09:15:49 +0000

    editing frontend files

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ const app = express();
 // Serve frontend folder
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-// Default route
+// Load index page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 const PORT = 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log("Server running on http://localhost:5000");
 });
